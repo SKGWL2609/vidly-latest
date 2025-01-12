fa0334a9ef4cc2899e6ba47cfa8cb24ceacce8e6
Shiv Kumar <skgwl2609@gmail.com>
2025-01-12 20:11:58 +0530

    converted Like into a Functional Component

--- src/components/common/Like.jsx
+++ src/components/common/Like.jsx
@@ -1,17 +1,21 @@
-import React, { Component } from "react";
-
 // input -> liked: true/false
 // output -> raise onClick event and toggle the liked
 
 // <i className="bi bi-heart"></i>
 // <i className="bi bi-heart-fill"></i>
 
-export default class Like extends Component {
-  render() {
-    let classes = "bi bi-heart";
-    if(this.props.liked) classes += '-fill'
-    return (
-        <i onClick={this.props.onClick} style={{cursor: 'pointer'}} className={classes}></i>
-    );
-  }
+import React from "react";
+
+const Like = props => {
+  let classes = "text-danger bi bi-heart";
+  if (props.liked) classes += "-fill";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+    ></i>
+  );
 }
+
+export default Like

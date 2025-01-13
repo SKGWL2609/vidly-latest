43dade2dce581fac23e084589eb2e6fe0a1ce5a9
Shiv Kumar <skgwl2609@gmail.com>
2025-01-13 18:00:12 +0530

    Sorting - Adding a Sort Icon

--- src/components/common/TableHeader.jsx
+++ src/components/common/TableHeader.jsx
@@ -12,13 +12,21 @@ export default class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = column => {
+    const { sortColumn } = this.props;
+
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className="bi bi-sort-alpha-down" />;
+    return <i className="bi bi-sort-alpha-up" />;
+  };
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map((column) => (
-            <th scope="col" key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>
-              {column.label}
+            <th className="clickable" scope="col" key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
